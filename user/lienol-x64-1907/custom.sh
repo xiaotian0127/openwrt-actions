e59c19d5f4ded8c9cfa4d654df2b2d587da28fef
xiaotian0127 <xiaotian0127@gmail.com>
2020-10-05 11:20:11 +0800

    Update custom.sh

--- user/lienol-x64-1907/custom.sh
+++ user/lienol-x64-1907/custom.sh
@@ -4,4 +4,4 @@ echo "Test custom.sh"
 
 git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
 git clone https://github.com/tty228/luci-app-serverchan.git       package/luci-app-serverchan
-
+git clone https://github.com/vernesong/OpenClash.git              package/OpenClash
